4a1a2d36631af2cbfa155df7ece2577e068962d1
Enrique Verea <kikeverea@gmail.com>
2025-11-11 16:42:41 +0100

    Add tailwind, playwright and tanstack dependencies

--- src/test/vitest.setup.tsx
+++ src/test/vitest.setup.tsx
@@ -3,6 +3,7 @@ import { server } from './server.ts'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
+
 beforeAll(() => server.listen())
 
 afterEach(() => {
@@ -11,3 +12,15 @@ afterEach(() => {
 })
 
 afterAll(() => server.close())
+
+
+vi.mock('@tanstack/react-router', async (importActual) => {
+  const actual = await importActual<any>()
+  return {
+    ...actual,
+    // Replace TanStack Router's Link with a plain anchor
+    Link: ({ to, children, ...props }: any) => (
+      <a href={to ? to : '/'} {...props}>{children}</a>
+    ),
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,22 @@ import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/test/vitest.setup.ts',
+    setupFiles: './src/test/vitest.setup.tsx',
     include: [ '**/*.{test,spec}.{js,ts,jsx,tsx}' ],
     exclude: [ ...configDefaults.exclude, 'node_modules', 'dist' ],
   },
